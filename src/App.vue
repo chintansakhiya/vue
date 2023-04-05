bad7401dda10290bf142a26015132385db13a666
chintan <chintan.sakhiya@improwised.com>
2023-04-05 17:48:38 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const counter = reactive({ count: 0 })
+
+const red= ref('colors')
+const count=ref(0)
+function incress() {
+count.value++  
+}
+</script>
+
+<template>
+  <h1 :class="red">Hello World!</h1>
+  <p>Count is: {{ count }}</p>
+  <button @click="incress">Count</button>
+</template>
+
+<style scoped>
+ .colors{
+  color: red;
+
+ }
+</style>
